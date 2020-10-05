210dbb5f03208eb0dca7b29a0315941d6dfbcbed
Berenice Herrera <bherrera@thoughtworks.com>
2020-10-05 19:40:56 -0300

    #86 [Bere] adds header title

--- ui/src/SymptomManagement/SymptomManagement.jsx
+++ ui/src/SymptomManagement/SymptomManagement.jsx
@@ -6,17 +6,17 @@ import { Route, Switch } from "react-router-dom";
 function SymptomManagement() {
   const [painValue, setPainValue] = useState("faceZero");
   return (
-      <Switch>
-        <Route exact path={"/"}>
-          <WelcomeSayu />
-        </Route>
-        <Route exact path={"/face-scale-screen"}>
-          <FaceScaleScreen painValue={painValue} setPainValue={setPainValue}/>
-        </Route>
-        <Route exact path={"/symptoms-registry"}>
-          <SymptomsRegistry painValue={painValue}/>
-        </Route>
-      </Switch>
+    <Switch>
+      <Route exact path={"/"}>
+        <WelcomeSayu />
+      </Route>
+      <Route exact path={"/face-scale-screen"}>
+        <FaceScaleScreen painValue={painValue} setPainValue={setPainValue} />
+      </Route>
+      <Route exact path={"/symptoms-registry"}>
+        <SymptomsRegistry painValue={painValue} />
+      </Route>
+    </Switch>
   );
 }
 export { SymptomManagement };
---
+++ ui/src/components/TitleHeader/TitleHeader.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Image, Box, Flex, Text } from "@chakra-ui/core";
+import BackArrow from '../../assets/images/BackArrow.svg';
+import { useHistory } from "react-router-dom";
+
+function TitleHeader(){
+    const history = useHistory();
+    return(
+    <>
+      <button onClick={() => {history.push("/");}}>
+            <Image src={BackArrow} />
+       </button>
+       <Text>Cuéntale a Sayu cómo te sientes hoy</Text>
+       <Text>Registro de dolor</Text>
+    </>
+    );
+}
+export {TitleHeader};
