6b18da02c724668d73c2ef526fc1076b13c0ede6
Berenice Herrera <bherrera@thoughtworks.com>
2020-10-06 16:58:11 -0300

    #86 [Kath, Bere] adds face scale screen style

--- ui/src/components/TitleHeader/TitleHeader.jsx
+++ ui/src/components/TitleHeader/TitleHeader.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import { Image, Box, Flex, Text } from "@chakra-ui/core";
+import { Image, Box, Flex, Text, Stack} from "@chakra-ui/core";
 import BackArrow from '../../assets/images/BackArrow.svg';
+import CloseIcon from '../../assets/images/CloseIcon.svg';
 import { useHistory } from "react-router-dom";
 
 function TitleHeader(){
     const history = useHistory();
     return(
     <>
+    <Flex justify="space-between">
       <button onClick={() => {history.push("/");}}>
             <Image src={BackArrow} />
        </button>
-       <Text>Cuéntale a Sayu cómo te sientes hoy</Text>
-       <Text fontWeight="bold">Registro de dolor</Text>
+       <button onClick={() => {history.push("/");}}>
+                   <Image src={CloseIcon} />
+       </button>
+     </Flex>
+     <Flex direction="column">
+       <Text marginTop={5}>Cuéntale a Sayu cómo te sientes hoy</Text>
+       <Text fontWeight="bold" marginTop={5}>Registro de dolor</Text>
+    </Flex>
     </>
+
     );
 }
 export {TitleHeader};
