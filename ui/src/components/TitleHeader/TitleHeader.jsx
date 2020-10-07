c302bfce89ed655070e2e0e8ca1a8063fac7cd38
Berenice Herrera <bherrera@thoughtworks.com>
2020-10-06 23:03:01 -0300

    #86 [Kath, Bere] formatting TitleHeader component

--- ui/src/components/TitleHeader/TitleHeader.jsx
+++ ui/src/components/TitleHeader/TitleHeader.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
-import { Image, Box, Flex, Text, Stack} from "@chakra-ui/core";
-import BackArrow from '../../assets/images/BackArrow.svg';
-import CloseIcon from '../../assets/images/CloseIcon.svg';
+import React from "react";
+import { Image, Box, Flex, Text, Stack } from "@chakra-ui/core";
+import BackArrow from "../../assets/images/BackArrow.svg";
+import CloseIcon from "../../assets/images/CloseIcon.svg";
 import { useHistory } from "react-router-dom";
 
-function TitleHeader(){
-    const history = useHistory();
-    return(
+function TitleHeader() {
+  const history = useHistory();
+  return (
     <>
-    <Flex justify="space-between">
-      <button onClick={() => {history.push("/");}}>
-            <Image src={BackArrow} />
-       </button>
-       <button onClick={() => {history.push("/");}}>
-                   <Image src={CloseIcon} />
-       </button>
-     </Flex>
-     <Flex direction="column">
-       <Text marginTop={5}>Cuéntale a Sayu cómo te sientes hoy</Text>
-       <Text fontWeight="bold" marginTop={5}>Registro de dolor</Text>
-    </Flex>
+      <Flex justify="space-between">
+        <button
+          onClick={() => {
+            history.push("/");
+          }}
+        >
+          <Image src={BackArrow} />
+        </button>
+        <button
+          onClick={() => {
+            history.push("/");
+          }}
+        >
+          <Image src={CloseIcon} />
+        </button>
+      </Flex>
+      <Flex direction="column">
+        <Text marginTop={5}>Cuéntale a Sayu cómo te sientes hoy</Text>
+        <Text fontWeight="bold" marginTop={5}>
+          Registro de dolor
+        </Text>
+      </Flex>
     </>
-
-    );
+  );
 }
-export {TitleHeader};
+export { TitleHeader };
