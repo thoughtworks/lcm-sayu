c85e38913fa243aeefb998cfd5db8acfc34962e1
Daniel Santibañez <dsantiba@thoughtworks.com>
2020-10-06 21:58:23 -0300

    #90 Adds style to slider

--- ui/src/components/SymptomSlider/SymptomSlider.jsx
+++ ui/src/components/SymptomSlider/SymptomSlider.jsx
@@ -6,34 +6,48 @@ import {
     SliderThumb,
     Box,
     Flex,
-    Text, FormControl, FormLabel
+    Text, FormControl, FormLabel, Stack
   } from "@chakra-ui/core";
+
   const symptonInfo={
-    'cansancio':{min:'Sin cansancio', max:'Máximo cansancio'},
-    'nausea':{min:'Sin náusea', max:'Máxima nausea'},
-    'depresion':{min:'Sin depresión', max:'Máxima depresión'},
-    'ansiedad':{min:'Sin ansiedad', max:'Máxima ansiedad'},
-    'somnolencia':{min:'Sin somnolencia', max:'Máxima somnolencia'},
-    'apetito':{min:'Buen apetito', max:'Sin apetito'},
-    'Bienestar/malestar':{min:'Sin malestar', max:'Máximo malestar'},
+    'Cansancio':{min:'Sin cansancio', max:'Máximo cansancio'},
+    'Nausea':{min:'Sin náusea', max:'Máxima nausea'},
+    'Depresion':{min:'Sin depresión', max:'Máxima depresión'},
+    'Ansiedad':{min:'Sin ansiedad', max:'Máxima ansiedad'},
+    'Somnolencia':{min:'Sin somnolencia', max:'Máxima somnolencia'},
+    'Apetito':{min:'Buen apetito', max:'Sin apetito'},
+    'Bienestar/Malestar':{min:'Máximo bienestar', max:'Máximo malestar'},
     'Falta de aire':{min:'Sin falta de aire', max:'Máxima falta de aire'},
     'Dificultad para dormir':{min:'Sin dificultad para dormir', max:'Máxima dificultad para dormir'}
   }
+  
   function Slider({symptomValue}){
     const [sliderValue, setSliderValue] = useState(0);
       return(
       <Box>
         <FormControl>
-          <FormLabel htmlFor={symptomValue}>{symptomValue}</FormLabel>
-          <ChakraSlider id={symptomValue} value={sliderValue} onChange={setSliderValue} min={0} max={10} step={2}>
-            <SliderTrack />
-            <SliderFilledTrack />
-            <SliderThumb size={8}>
-              <Box>{sliderValue}</Box>
-            </SliderThumb>
-          </ChakraSlider>
-          <Flex justifyContent={'space-between'}>
-          <Text as="sub">{symptonInfo[symptomValue].min}</Text> <Text as="sub">{symptonInfo[symptomValue].max}</Text>
+          <FormLabel htmlFor={symptomValue}>
+            <Stack isInline>
+              <Text fontFamily="heading">
+                {symptomValue}
+              </Text>
+              <Text color="lightPurple">
+                {sliderValue}
+              </Text>
+            </Stack>
+          </FormLabel>
+          <Stack isInline spacing={2}>
+            <Text>0</Text>
+            <ChakraSlider id={symptomValue} value={sliderValue} onChange={setSliderValue} min={0} max={10} step={1}>
+              <SliderTrack h={1} />
+              <SliderFilledTrack />
+              <SliderThumb size={4} bg="lightPurple" />
+            </ChakraSlider>
+            <Text>10</Text>
+          </Stack>
+          <Flex justifyContent={'space-between'} mt={5}>
+            <Text as="sub" color="lightGrey">{symptonInfo[symptomValue].min}</Text>
+            <Text as="sub" color="lightGrey">{symptonInfo[symptomValue].max}</Text>
           </Flex>
         </FormControl>
       </Box>);
