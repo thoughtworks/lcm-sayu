8575785f6ba0669b142ed2fc9040f0e1cf7bcc73
Daniel Santibañez <dsantiba@thoughtworks.com>
2020-10-14 18:03:27 -0300

    #90 Refactor test

--- ui/test/components/SymptomSlider/SymptomSlider.test.jsx
+++ ui/test/components/SymptomSlider/SymptomSlider.test.jsx
@@ -4,21 +4,25 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import { ThemeProvider } from '@chakra-ui/core';
 import "regenerator-runtime/runtime";
 
-test("Should change symptom level value when it moves to right side", async()=>{
-    render(<ThemeProvider><SymtomSlider symptomValue="Náusea"/></ThemeProvider>);
-    const sliderButton =screen.getByRole('slider');
-    fireEvent.keyDown(sliderButton, { key: 'ArrowRight', code: 'ArrowRight' })
-    fireEvent.keyDown(sliderButton, { key: 'ArrowRight', code: 'ArrowRight' })
-    fireEvent.keyDown(sliderButton, { key: 'ArrowRight', code: 'ArrowRight' })
-    expect(await screen.findByText('3')).toBeVisible();
-});
+describe("<SymtomSlider/>", () => {
+    beforeEach(()=>{
+        render(<ThemeProvider><SymtomSlider symptomValue="Náusea"/></ThemeProvider>);
+      });
 
-test("Should show the right symptom text", async()=>{
-    render(<ThemeProvider><SymtomSlider symptomValue="Náusea"/></ThemeProvider>);
-    const nauseaText = screen.getByText(/^Náusea$/i);
-    expect(nauseaText).toBeInTheDocument();
-    const minNauseaText = screen.getByText(/Sin náusea/i);
-    expect(minNauseaText).toBeInTheDocument()
-    const maxNauseaText = screen.getByText(/Máxima náusea/i);
-    expect(maxNauseaText).toBeInTheDocument()
-});
+    test("Should change symptom level value when it moves to right side", async()=>{
+        const sliderButton =screen.getByRole('slider');
+        fireEvent.keyDown(sliderButton, { key: 'ArrowRight', code: 'ArrowRight' })
+        fireEvent.keyDown(sliderButton, { key: 'ArrowRight', code: 'ArrowRight' })
+        fireEvent.keyDown(sliderButton, { key: 'ArrowRight', code: 'ArrowRight' })
+        expect(await screen.findByText('3')).toBeVisible();
+    });
+
+    test("Should show the right symptom text", async()=>{
+        const nauseaText = screen.getByText(/^Náusea$/i);
+        expect(nauseaText).toBeInTheDocument();
+        const minNauseaText = screen.getByText(/Sin náusea/i);
+        expect(minNauseaText).toBeInTheDocument()
+        const maxNauseaText = screen.getByText(/Máxima náusea/i);
+        expect(maxNauseaText).toBeInTheDocument()
+    });
+})
