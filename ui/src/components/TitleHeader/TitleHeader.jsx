c9ecc91d4cf21c0dc47025cbe53fb593457c06bb
Berenice Herrera <bherrera@thoughtworks.com>
2020-10-19 10:25:37 -0300

    [Bere] remove close icon from title heaqder component

--- ui/src/components/TitleHeader/TitleHeader.jsx
+++ ui/src/components/TitleHeader/TitleHeader.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Image, Box, Flex, Text, Stack } from "@chakra-ui/core";
 import BackArrow from "../../assets/images/BackArrow.svg";
-import CloseIcon from "../../assets/images/CloseIcon.svg";
 import { useHistory } from "react-router-dom";
 
 function TitleHeader(props) {
   const history = useHistory();
   return (
     <Box marginTop={5}>
-      <Flex justify="space-between">
         <button
           onClick={() => {
             history.push(props.backArrowRoute);
@@ -16,14 +14,6 @@ function TitleHeader(props) {
         >
           <Image src={BackArrow} />
         </button>
-        <button
-          onClick={() => {
-            history.push(props.closeRoute);
-          }}
-        >
-          <Image src={CloseIcon} />
-        </button>
-      </Flex>
       <Flex direction="column">
         <Text marginTop={5} fontSize={["md", "lg", "lg", "xl"]}>
           Cuéntale a sayu cómo te sientes hoy
