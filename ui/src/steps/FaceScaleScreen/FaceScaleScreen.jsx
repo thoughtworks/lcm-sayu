7617c9a4a0d28b74df12933bd031c4ba86f60b43
Berenice Herrera <bherrera@thoughtworks.com>
2020-10-19 14:55:50 -0300

    [Dani C, Bere] fixing styles props

--- ui/src/steps/FaceScaleScreen/FaceScaleScreen.jsx
+++ ui/src/steps/FaceScaleScreen/FaceScaleScreen.jsx
@@ -9,20 +9,21 @@ function FaceScaleScreen({ setPainValue }) {
   return (
     <>
       <TitleHeader backArrowRoute="/" closeRoute="/" />
-      <Box>
+
         <Text fontSize={["sm"]}>
           Muéstrale a tu hijo/hija este dibujo y explícale lo siguiente: "Elige
           la cara que mejor describa cuánto te duele ahora"
         </Text>
-        <Box marginTop={20}>
+        <Stack marginTop={20}
+          width="100%"
+          align="center">
           <Text fontWeight="bold"
            fontSize={["sm"]}
-           align="center"
            marginBottom={5}>
             Won-Baker FACES® Pain Rating Scale
           </Text>
-        </Box>
-        <Stack isInline>
+        </Stack>
+        <Stack isInline width="100%" justifyContent="center">
           <FaceButton painValue="faceZero" setPainValue={setPainValue} />
           <FaceButton painValue="faceTwo" setPainValue={setPainValue} />
           <FaceButton painValue="faceFour" setPainValue={setPainValue} />
@@ -30,11 +31,14 @@ function FaceScaleScreen({ setPainValue }) {
           <FaceButton painValue="faceEight" setPainValue={setPainValue} />
           <FaceButton painValue="faceTen" setPainValue={setPainValue} />
         </Stack>
+        <Stack
+          width="100%"
+          align="center">
           <Text  fontSize={["sm"]}
            marginTop={20}>
             © 1983 Wong-Baker FACES Foundation. www.WongBakerFACES.org  Used with permission.
           </Text>
-      </Box>
+        </Stack>
     </>
   );
 }
