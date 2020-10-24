4308b9964b80144a44ef8c6c030e394f5ef0d6c9
Daniel Santibañez <dsantiba@thoughtworks.com>
2020-10-23 21:44:57 -0300

    [Dani C Dani S] 130 Adds radio in line layout and variant color

--- ui/src/components/SymptomRadioButton/SymptomRadioButton.jsx
+++ ui/src/components/SymptomRadioButton/SymptomRadioButton.jsx
@@ -8,17 +8,15 @@ import {
     'Fiebre':{name:'Fiebre', description: '¿Ha tenido en las últimas 24 horas fiebre?'}
   }
 
-function SymptomRadioButton({symptomValue}) {
+function SymptomRadioButton({symptomValue, symptomName}) {
     const [value, setValue] = React.useState("0");
     return (
     <>
       <Text fontFamily="heading">{symptonInfo[symptomValue].name}</Text>
       <Text fontSize="xs" color="lightGrey">{symptonInfo[symptomValue].description}</Text>
-      <RadioGroup onChange={e => setValue(e.target.value)} value={value}>
-        <Stack isInline>
-            <Radio value="1">Sí</Radio>
-            <Radio value="0">No</Radio>
-        </Stack>
+      <RadioGroup isInline onChange={e => setValue(e.target.value)} value={value}>
+        <Radio variantColor="blue" value="1">Sí</Radio>
+        <Radio variantColor="blue" value="0">No</Radio>
       </RadioGroup>
     </>
     );
