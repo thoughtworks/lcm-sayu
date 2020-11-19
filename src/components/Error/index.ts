58cacecb0d412759167f5ca1fd6b30b5ac2f7f0f
Benjamín <brokarth@gmail.com>
2020-11-19 15:57:28 -0300

    [@benja] Move ErrorCode into a different file

--- src/components/Error/index.ts
+++ src/components/Error/index.ts
@@ -2,9 +2,4 @@ export { GenericError } from './GenericError'
 export { AuthenticationDeniedError } from './AutheticationDeniedError'
 export { Unauthorized } from './Unauthorized'
 export { FailedSymptomsRegistry } from './FailedSymptomsRegistry'
-
-export enum ErrorCodes {
-  AccessDenied = 'AccessDenied',
-  Unauthorized = 'Unauthorized',
-  FailedSymptomsRegistry = 'FailedSymptomsRegistry',
-}
+export { ErrorCodes } from './ErrorCodes'
