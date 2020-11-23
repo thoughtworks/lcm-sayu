05284aff37c667f655107d60faedf67f691161bd
Berenice Herrera <bherrera@thoughtworks.com>
2020-11-23 13:17:56 -0300

    #159[Bere, Benja] adding styles to AddUser form

--- src/steps/AddUser/AddUser.tsx
+++ src/steps/AddUser/AddUser.tsx
@@ -1,18 +1,20 @@
-import axios from 'axios'
 import { useRouter } from 'next/router'
-import { useForm } from 'react-hook-form'
+import axios from 'axios'
+import { useForm, Controller } from 'react-hook-form'
+import { Radio, RadioGroup } from '@chakra-ui/core'
 import { SubmitButton } from 'src/components/SubmitButton'
 import { TitleHeader } from 'src/components/TitleHeader/TitleHeader'
 import { Role } from 'src/model/Role'
+import styles from './AddUser.module.scss'
 
 const AddUser = () => {
-  const { handleSubmit, register } = useForm()
+  const { handleSubmit, register, control } = useForm()
   const router = useRouter()
 
   return (
     <>
-      <TitleHeader />
-      <main>
+      <main id={styles['add-user']}>
+        <TitleHeader />
         <h1>Agregar usuario</h1>
         <form
           onSubmit={handleSubmit(async ({ userMail, role }: any) => {
@@ -25,30 +27,44 @@ const AddUser = () => {
             router.push('/registro-exitoso-usuario')
           })}
         >
-          <div>
+          <div className={styles['user-email']}>
             <label htmlFor="userEmail">Correo electrónico</label>
             <input id="userEmail" name="userMail" type="email" ref={register} />
           </div>
-          <div>
+          <div className={styles['role']}>
             <h2>Rol de la persona</h2>
-            <div>
-              <label htmlFor="tratante">Profesional tratante</label>
-              <input
-                id="tratante"
+            <div className={styles['role-values']}>
+              <Controller
+                control={control}
                 name="role"
-                type="radio"
-                value={Role.TRATANTE}
-                ref={register}
-              />
-            </div>
-            <div>
-              <label htmlFor="cuidador">Cuidadora</label>
-              <input
-                id="cuidador"
-                name="role"
-                type="radio"
-                value={Role.CUIDADOR}
-                ref={register}
+                defaultValue={Role.CUIDADOR}
+                render={({ onChange, onBlur, value }) => (
+                  <RadioGroup
+                    isInline
+                    onChange={(_, value) => onChange(value)}
+                    value={value}
+                    onBlur={onBlur}
+                  >
+                    <Radio
+                      id={Role.TRATANTE}
+                      variantColor="blue"
+                      value={Role.TRATANTE}
+                      size="lg"
+                    >
+                      <span className={styles['role-value']}>
+                        Profesional tratante
+                      </span>
+                    </Radio>
+                    <Radio
+                      id={Role.CUIDADOR}
+                      variantColor="blue"
+                      value={Role.CUIDADOR}
+                      size="lg"
+                    >
+                      <span className={styles['role-value']}>Cuidadora</span>
+                    </Radio>
+                  </RadioGroup>
+                )}
               />
             </div>
           </div>
