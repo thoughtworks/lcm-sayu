a18ee3b241fa6b407c9fd6c1cb4b48ebf85081e3
dani <daniela.cortes@thoughtworks.com>
2020-11-26 13:41:24 -0300

    [Dani S, Dani C] fixed date formatting function

--- src/steps/SymptomsRegistryList.tsx
+++ src/steps/SymptomsRegistryList.tsx
@@ -10,6 +10,7 @@ import { ErrorCodes } from 'src/components/Error'
 
 import { RegistryDTO } from 'src/dto/RegistryDTO'
 import { RegistryService } from 'src/services/RegistryService'
+import { Navigation } from 'src/components/Navigation/Navigation'
 
 export type ViewRegistry = {
   day: number
@@ -34,40 +35,45 @@ const SymptomsRegistryList: FunctionComponent<SymptomsRegistryListProp> = ({
 
   return (
     <>
-      <Text fontSize={['lg']}>Historial de síntomas</Text>
+      <Navigation />
+      <Text fontSize={['lg']} mt={2}>
+        Historial de síntomas
+      </Text>
 
       <Stack align="center" mt={2} spacing={3}>
         <Box>
           <SymptomsLegend />
         </Box>
-        <Text fontSize={['lg']} width={277} textAlign="left">
+        <Text fontSize={['lg']} textAlign="left" width="100%">
           {viewRegistries?.length != 0
             ? formatMonthAndYear(viewRegistries?.slice(0, 1)[0].day)
             : ''}
         </Text>
-        {viewRegistries?.length != 0 ? (
-          viewRegistries?.map(({ day, registries }) => (
-            <div key={day}>
-              <DateBox symptomDate={formatDayAndNumberDate(day)} />
-              {registries.map((registry) => (
-                <SymptomsDailyValues
-                  key={registry.id}
-                  symptomDate={formatHourAndMinutes(registry.symptomDate)}
-                  painLevel={registry.painLevel}
-                  tireLevel={registry.tireLevel}
-                  appetiteLevel={registry.appetiteLevel}
-                  nauseaLevel={registry.nauseaLevel}
-                  swallowLevel={registry.swallowLevel}
-                  airLevel={registry.airLevel}
-                  depositionLevel={registry.depositionLevel}
-                  feverLevel={registry.feverLevel}
-                />
-              ))}
-            </div>
-          ))
-        ) : (
-          <Text>Aún no tienes registros</Text>
-        )}
+        <Box width="100%" alignItems="center" alignContent="center">
+          {viewRegistries?.length != 0 ? (
+            viewRegistries?.map(({ day, registries }) => (
+              <div key={day}>
+                <DateBox symptomDate={formatDayAndNumberDate(day)} />
+                {registries.map((registry) => (
+                  <SymptomsDailyValues
+                    key={registry.id}
+                    symptomDate={formatHourAndMinutes(registry.symptomDate)}
+                    painLevel={registry.painLevel}
+                    tireLevel={registry.tireLevel}
+                    appetiteLevel={registry.appetiteLevel}
+                    nauseaLevel={registry.nauseaLevel}
+                    swallowLevel={registry.swallowLevel}
+                    airLevel={registry.airLevel}
+                    depositionLevel={registry.depositionLevel}
+                    feverLevel={registry.feverLevel}
+                  />
+                ))}
+              </div>
+            ))
+          ) : (
+            <Text>Aún no tienes registros</Text>
+          )}
+        </Box>
       </Stack>
     </>
   )
@@ -137,85 +143,41 @@ const formatHourAndMinutes = (unformattedDate: number) => {
 }
 
 const formatDayAndNumberDate = (unformattedDate: number) => {
-  let formattedDay: string
   const date = new Date(unformattedDate)
   const day = date.getDay()
   const numberDate = date.getDate()
-  switch (day) {
-    case 0:
-      formattedDay = 'Domingo'
-      break
-    case 1:
-      formattedDay = 'Lunes'
-      break
-    case 2:
-      formattedDay = 'Martes'
-      break
-    case 3:
-      formattedDay = 'Miércoles'
-      break
-    case 4:
-      formattedDay = 'Jueves'
-      break
-    case 5:
-      formattedDay = 'Viernes'
-      break
-    case 6:
-      formattedDay = 'Sábado'
-      break
-    default:
-      formattedDay = ''
-  }
-  const formattedDate = formattedDay + ',' + numberDate
+  const dayValues = [
+    'Domingo',
+    'Lunes',
+    'Martes',
+    'Miércoles',
+    'Jueves',
+    'Viernes',
+    'Sábado',
+  ]
+  const formattedDate = dayValues[day] + ',' + numberDate
   return formattedDate
 }
 
 const formatMonthAndYear = (unformattedDate: number) => {
-  let formattedMonth: string
   const date = new Date(unformattedDate)
   const month = date.getMonth()
   const year = date.getFullYear()
-  switch (month) {
-    case 0:
-      formattedMonth = 'Enero'
-      break
-    case 1:
-      formattedMonth = 'Febrero'
-      break
-    case 2:
-      formattedMonth = 'Marzo'
-      break
-    case 3:
-      formattedMonth = 'Abril'
-      break
-    case 4:
-      formattedMonth = 'Mayo'
-      break
-    case 5:
-      formattedMonth = 'Junio'
-      break
-    case 6:
-      formattedMonth = 'Julio'
-      break
-    case 7:
-      formattedMonth = 'Agosto'
-      break
-    case 8:
-      formattedMonth = 'Septiembre'
-      break
-    case 9:
-      formattedMonth = 'Octubre'
-      break
-    case 10:
-      formattedMonth = 'Noviembre'
-      break
-    case 11:
-      formattedMonth = 'Diciembre'
-      break
-    default:
-      formattedMonth = ''
-  }
-  const formattedDate = formattedMonth + ', ' + year
+  const monthValues = [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre',
+  ]
+  const formattedDate = monthValues[month] + ', ' + year
   return formattedDate
 }
 
