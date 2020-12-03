3c9d2a286c8fc57fe12ec4369258f41edbc86cb3
Daniel Santibañez <dsantiba@thoughtworks.com>
2020-12-03 15:32:43 -0300

    [Bere, DaniS] #80 WIP Resolving PR comments

--- src/components/SymptomsDailyValues/SymptomsDailyValues.tsx
+++ src/components/SymptomsDailyValues/SymptomsDailyValues.tsx
@@ -28,56 +28,54 @@ const SymptomsDailyValues = ({
   rescueLevel,
 }: SymptomsLevel) => (
   <div
-    className={
-      rescueLevel
-        ? styles['symptom-list'] + ' ' + styles['with-rescue']
-        : styles['symptom-list']
-    }
+    className={`${styles['symptom-list']} ${
+      rescueLevel && styles['with-rescue']
+    }`}
   >
     <Text fontSize={'xs'} ml={1} lineHeight={'tall'}>
       {symptomDate}
     </Text>
     <Box ml={1}>
-      <div className={styles['dolor-value'] + ' ' + styles['symptom-circle']}>
+      <div className={`${styles['dolor-value']} ${styles['symptom-circle']}`}>
         {painLevel}
       </div>
     </Box>
     <Box>
       <div
-        className={styles['cansancio-value'] + ' ' + styles['symptom-circle']}
+        className={`${styles['cansancio-value']} ${styles['symptom-circle']}`}
       >
         {tireLevel}
       </div>
     </Box>
     <Box>
-      <div className={styles['apetito-value'] + ' ' + styles['symptom-circle']}>
+      <div className={`${styles['apetito-value']} ${styles['symptom-circle']}`}>
         {appetiteLevel}
       </div>
     </Box>
     <Box>
-      <div className={styles['nauseas-value'] + ' ' + styles['symptom-circle']}>
+      <div className={`${styles['nauseas-value']} ${styles['symptom-circle']}`}>
         {nauseaLevel}
       </div>
     </Box>
     <Box>
-      <div className={styles['tragar-value'] + ' ' + styles['symptom-circle']}>
+      <div className={`${styles['tragar-value']} ${styles['symptom-circle']}`}>
         {swallowLevel}
       </div>
     </Box>
     <Box>
-      <div className={styles['aire-value'] + ' ' + styles['symptom-circle']}>
+      <div className={`${styles['aire-value']} ${styles['symptom-circle']}`}>
         {airLevel}
       </div>
     </Box>
     <Box>
       <div
-        className={styles['deposicion-value'] + ' ' + styles['symptom-circle']}
+        className={`${styles['deposicion-value']} ${styles['symptom-circle']}`}
       >
         {depositionLevel ? 'SI' : 'NO'}
       </div>
     </Box>
     <Box>
-      <div className={styles['fiebre-value'] + ' ' + styles['symptom-circle']}>
+      <div className={`${styles['fiebre-value']} ${styles['symptom-circle']}`}>
         {feverLevel ? 'SI' : 'NO'}
       </div>
     </Box>
