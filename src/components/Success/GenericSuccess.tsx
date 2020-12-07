d5d27d5e156674e867eee172a01102ccf0f6ecff
Berenice Herrera <bherrera@thoughtworks.com>
2020-12-07 13:27:06 -0300

    #[DaniS, Bere] adding button to go back to symptoms history

--- src/components/Success/GenericSuccess.tsx
+++ src/components/Success/GenericSuccess.tsx
@@ -3,13 +3,22 @@ import ButtonLink from 'src/components/ButtonLink'
 
 import styles from './GenericSuccess.module.scss'
 
-const GenericSuccess: FunctionComponent<{ msg?: string }> = ({
-  msg = '¡Todo ha salido bien! ✅',
-}) => (
-  <main id={styles['success']}>
+const GenericSuccess: FunctionComponent<{
+  msg?: string
+  msgButton?: string
+  urlButton?: string
+}> = ({ msg = '¡Todo ha salido bien! ✅', msgButton, urlButton }) => (
+  <main id={styles.success}>
     <img src="/img/checkmark.svg" alt="éxito" width={122} height={122} />
     <p>{msg}</p>
-    <ButtonLink href="/" label="Volver al inicio" />
+    {urlButton && msgButton && (
+      <ButtonLink href={urlButton} label={msgButton} />
+    )}
+    <ButtonLink
+      href="/"
+      label="Volver al inicio"
+      secondaryStyle={urlButton && msgButton ? true : false}
+    />
   </main>
 )
 
--- src/components/Success/SuccessfulSymptomRegistry.tsx
+++ src/components/Success/SuccessfulSymptomRegistry.tsx
@@ -2,7 +2,11 @@ import { FunctionComponent } from 'react'
 import { GenericSuccess } from './GenericSuccess'
 
 const SuccessfulSymptomRegistry: FunctionComponent = () => (
-  <GenericSuccess msg="¡Se han guardado los síntomas exitosamente!" />
+  <GenericSuccess
+    msg="¡Se han guardado los síntomas exitosamente!"
+    msgButton="Ir a historial de síntomas"
+    urlButton="/ver-registros-sintomas"
+  />
 )
 
 export { SuccessfulSymptomRegistry }
