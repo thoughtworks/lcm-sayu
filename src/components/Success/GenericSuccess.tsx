1ee684bb91811a19bacff6de012cc83edf5338bd
Berenice Herrera <bherrera@thoughtworks.com>
2020-12-14 18:27:44 -0300

    #161[DaniC, DaniS, Bere] adding second button wuth params to success page

--- src/components/Success/GenericSuccess.tsx
+++ src/components/Success/GenericSuccess.tsx
@@ -3,13 +3,20 @@ import ButtonLink from 'src/components/ButtonLink'
 
 import styles from './GenericSuccess.module.scss'
 
-const GenericSuccess: FunctionComponent<{ msg?: string }> = ({
-  msg = '¡Todo ha salido bien! ✅',
-}) => (
+const GenericSuccess: FunctionComponent<{
+  msg?: string
+  backUrl?: string
+  backLabel?: string
+}> = ({ msg = '¡Todo ha salido bien! ✅', backUrl, backLabel }) => (
   <main id={styles['success']}>
     <img src="/img/checkmark.svg" alt="éxito" width={122} height={122} />
     <p>{msg}</p>
-    <ButtonLink href="/" label="Volver al inicio" />
+    {backUrl && (
+      <div className={styles.back}>
+        <ButtonLink href={backUrl} label={`Volver a  ${backLabel}`} />
+      </div>
+    )}
+    <ButtonLink href="/" label="Volver al inicio" secondaryStyle />
   </main>
 )
 
