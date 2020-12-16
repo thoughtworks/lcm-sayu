c33c86ae557ded29e6ba17a1174c7f0b1a397ffe
Benjamín <brokarth@gmail.com>
2020-12-16 17:56:43 -0300

    [Dani C., Benjamin] Resize day title

--- src/components/SymptomsDailyValues/SymptomsDailyValues.tsx
+++ src/components/SymptomsDailyValues/SymptomsDailyValues.tsx
@@ -1,6 +1,10 @@
-import styles from './symptomsDailyValues.module.scss'
 import React from 'react'
-import { Box, Text } from '@chakra-ui/core'
+import { Box, Icon, Text } from '@chakra-ui/core'
+import { useSession } from 'next-auth/client'
+
+import { Role } from 'src/model/Role'
+
+import styles from './symptomsDailyValues.module.scss'
 
 type SymptomsLevel = {
   symptomDate: string
@@ -26,60 +30,79 @@ const SymptomsDailyValues = ({
   depositionLevel,
   feverLevel,
   rescueLevel,
-}: SymptomsLevel) => (
-  <div
-    className={`${styles['symptom-list']} ${
-      rescueLevel && styles['with-rescue']
-    }`}
-  >
-    <Text fontSize={'xs'} ml={1} lineHeight={'tall'}>
-      {symptomDate}
-    </Text>
-    <Box ml={1}>
-      <div className={`${styles['dolor-value']} ${styles['symptom-circle']}`}>
-        {painLevel}
-      </div>
-    </Box>
-    <Box>
-      <div
-        className={`${styles['cansancio-value']} ${styles['symptom-circle']}`}
-      >
-        {tireLevel}
-      </div>
-    </Box>
-    <Box>
-      <div className={`${styles['apetito-value']} ${styles['symptom-circle']}`}>
-        {appetiteLevel}
-      </div>
-    </Box>
-    <Box>
-      <div className={`${styles['nauseas-value']} ${styles['symptom-circle']}`}>
-        {nauseaLevel}
-      </div>
-    </Box>
-    <Box>
-      <div className={`${styles['tragar-value']} ${styles['symptom-circle']}`}>
-        {swallowLevel}
-      </div>
-    </Box>
-    <Box>
-      <div className={`${styles['aire-value']} ${styles['symptom-circle']}`}>
-        {airLevel}
-      </div>
-    </Box>
-    <Box>
-      <div
-        className={`${styles['deposicion-value']} ${styles['symptom-circle']}`}
-      >
-        {depositionLevel ? 'SI' : 'NO'}
-      </div>
-    </Box>
-    <Box>
-      <div className={`${styles['fiebre-value']} ${styles['symptom-circle']}`}>
-        {feverLevel ? 'SI' : 'NO'}
-      </div>
-    </Box>
-  </div>
-)
+}: SymptomsLevel) => {
+  const [session] = useSession()
+
+  return (
+    <div
+      className={`${styles['symptom-list']} ${
+        rescueLevel && styles['with-rescue']
+      }`}
+    >
+      <Text fontSize={'xs'} ml={1} lineHeight={'tall'}>
+        {symptomDate}
+      </Text>
+      <Box ml={1}>
+        <div className={`${styles['dolor-value']} ${styles['symptom-circle']}`}>
+          {painLevel}
+        </div>
+      </Box>
+      <Box>
+        <div
+          className={`${styles['cansancio-value']} ${styles['symptom-circle']}`}
+        >
+          {tireLevel}
+        </div>
+      </Box>
+      <Box>
+        <div
+          className={`${styles['apetito-value']} ${styles['symptom-circle']}`}
+        >
+          {appetiteLevel}
+        </div>
+      </Box>
+      <Box>
+        <div
+          className={`${styles['nauseas-value']} ${styles['symptom-circle']}`}
+        >
+          {nauseaLevel}
+        </div>
+      </Box>
+      <Box>
+        <div
+          className={`${styles['tragar-value']} ${styles['symptom-circle']}`}
+        >
+          {swallowLevel}
+        </div>
+      </Box>
+      <Box>
+        <div className={`${styles['aire-value']} ${styles['symptom-circle']}`}>
+          {airLevel}
+        </div>
+      </Box>
+      <Box>
+        <div
+          className={`${styles['deposicion-value']} ${styles['symptom-circle']}`}
+        >
+          {depositionLevel ? 'SI' : 'NO'}
+        </div>
+      </Box>
+      <Box>
+        <div
+          className={`${styles['fiebre-value']} ${styles['symptom-circle']}`}
+        >
+          {feverLevel ? 'SI' : 'NO'}
+        </div>
+      </Box>
+      {session?.role === Role.CUIDADOR && (
+        <Box>
+          <div>
+            <Icon name="delete" />
+          </div>
+        </Box>
+      )}
+    </div>
+  )
+}
 
 export default SymptomsDailyValues
